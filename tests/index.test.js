a96146ae1481bafc00b81437a2b229967bcadc19
David Francisco <dmfrancisc@gmail.com>
2017-06-10 22:22:44 +0100

    Add some tests using jest and enzyme

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,38 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+import React from 'react';
+import { shallow } from 'enzyme';
+import MarkdownIt from 'markdown-it';
+
+import MarkdownTextarea from '../src';
+
+const markdown = new MarkdownIt();
+
+it('renders correctly with only required props', () => {
+  const element = shallow(
+    <MarkdownTextarea
+      render={value => markdown.render(value)}
+    />
+  );
+  expect(element.getNodes()).toMatchSnapshot();
+});
+
+it('displays toolbar on focus', () => {
+  const element = shallow(
+    <MarkdownTextarea
+      render={value => markdown.render(value)}
+    />
+  );
+  element.find('.MarkdownTextarea-write').simulate('focus');
+  expect(element.getNodes()).toMatchSnapshot();
+});
+
+it('supports preview', () => {
+  const element = shallow(
+    <MarkdownTextarea
+      value="Hello _World_"
+      render={value => markdown.render(value)}
+    />
+  );
+  element.find('.MarkdownTextarea-write').simulate('focus');
+  element.find('.MarkdownTextarea-action').at(1).simulate('click');
+  expect(element.getNodes()).toMatchSnapshot();
 });
---
+++ tests/toolbar_controller.test.js
@@ -0,0 +1,142 @@
+/* eslint-env jest */
+
+/* Tests adapted from: https://git.io/vHPOB */
+import MarkdownToolbar from '../src/toolbar_controller';
+
+it('should render prefixes correctly', () => {
+  const text = 'abcdefg';
+  const data = {
+    prefix: '**',
+  };
+  const rendered = '**abcdefg';
+  const c = new MarkdownToolbar();
+
+  expect(c.renderPrefix(0, 7, data, text)).toEqual(rendered);
+});
+
+it('should render suffixes correctly', () => {
+  const text = 'abcdefg';
+  const data = {
+    suffix: '**',
+  };
+  const rendered = 'abcdefg**';
+  const c = new MarkdownToolbar();
+
+  expect(c.renderSuffix(0, 7, 2, data, text)).toEqual(rendered);
+});
+
+it('should render bold selections correctly', () => {
+  let text = 'abcdef';
+  const data = {
+    prefix: '**',
+    suffix: '**',
+  };
+  let rendered = 'abcdef****';
+  let c = new MarkdownToolbar();
+  expect(c.render(data, 6, 6, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(8);
+  expect(c.selectionEnd).toEqual(8);
+
+  text = 'abcdef';
+  rendered = '**abcdef**';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, 6, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(2);
+  expect(c.selectionEnd).toEqual(8);
+
+  text = 'abcdef\nabcdef\n';
+  rendered = '**abcdef**\nabcdef\n';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, 6, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(2);
+  expect(c.selectionEnd).toEqual(8);
+
+  text = 'abcdef\nabcdef\n';
+  rendered = '**abcdef\nabcdef**\n';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, 13, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(2);
+  expect(c.selectionEnd).toEqual(15);
+});
+
+it('should render ul lists correctly', () => {
+  let text = 'abcdef';
+  const data = {
+    prefix: '- ',
+    multiline: true,
+  };
+  let rendered = 'abcdef- ';
+  let c = new MarkdownToolbar();
+  expect(c.render(data, text.length, text.length, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+
+  text = 'abcdef';
+  rendered = '- abcdef';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, text.length, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+
+  text = 'abcdef\nabcdef';
+  rendered = '- abcdef\n- abcdef';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, 13, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+
+  text = 'abcdef\nabcdef\n';
+  rendered = '- abcdef\n- abcdef\n- ';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, text.length, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+
+  text = 'abcd\nabcd\nabc';
+  rendered = '- abcd\n- abcd\n- abc';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, text.length, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+});
+
+it('should render ol lists correctly', () => {
+  let text = 'abcdef';
+  const data = {
+    prefix: '1. ',
+    multiline: true,
+  };
+  let rendered = 'abcdef1. ';
+  let c = new MarkdownToolbar();
+  expect(c.render(data, text.length, text.length, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+
+  text = 'abcdef';
+  rendered = '1. abcdef';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, text.length, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+
+  text = 'abcdef\nabcdef';
+  rendered = '1. abcdef\n2. abcdef';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, 13, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+
+  text = 'abcdef\nabcdef\n';
+  rendered = '1. abcdef\n2. abcdef\n3. ';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, text.length, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+
+  text = 'abcd\nabcd\nabc';
+  rendered = '1. abcd\n2. abcd\n3. abc';
+  c = new MarkdownToolbar();
+  expect(c.render(data, 0, text.length, text)).toEqual(rendered);
+  expect(c.selectionStart).toEqual(0);
+  expect(c.selectionEnd).toEqual(0);
+});
